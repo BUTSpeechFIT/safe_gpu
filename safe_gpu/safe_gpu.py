e65f808c6cce04fa0c55036a7ea0ddc16cc45fd1
Benes Karel <ibenes@fit.vutbr.cz>
2022-02-18 16:07:07 +0100

    Rework the way the list of free GPUs is obtained

--- safe_gpu/safe_gpu.py
+++ safe_gpu/safe_gpu.py
@@ -17,9 +17,25 @@ def get_free_gpus():
 
         Note that this information is volatile.
     '''
-    raw_output = subprocess.check_output("nvidia-smi -q | grep 'UUID\|Processes' | grep -B1 'None' | tr -d ' ' | grep 'UUID' | cut -d':' -f 2", shell=True)
+    gpus_smi = subprocess.check_output(["nvidia-smi", "--format=csv,noheader", "--query-gpu=index,gpu_bus_id"]).decode().strip()
+    processes_smi = subprocess.check_output(["nvidia-smi", "--format=csv,noheader", "--query-compute-apps=pid,gpu_bus_id"]).decode().strip()
 
-    return raw_output.decode().split()
+    gpus = {}
+    for line in gpus_smi.split('\n'):
+        idx, uuid =  line.split(',')
+        gpus[uuid.strip()] = idx.strip()
+
+    # in case of no running processes
+    if processes_smi == '':
+        return list(gpus.values())
+
+    used_gpus = set()
+    for line in processes_smi.split('\n'):
+        _, uuid = line.split(',')
+        used_gpus.add(uuid.strip())
+
+    free_gpus = list(set(gpus.keys()) - used_gpus)
+    return sorted([gpus[free] for free in free_gpus])
 
 
 def pytorch_placeholder(device_no):
